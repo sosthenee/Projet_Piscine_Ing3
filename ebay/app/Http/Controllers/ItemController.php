d9dc9117e018e41fce9ac992332d6b00cd4bbe50
Willy <6wmartin@gmail.com>
2020-04-14 13:04:46 +0200

    Affichage item + Installation des packages
    amélioration de l'affichage des items
    installations des packages l'ensemble du projet -> commande 'npm install', 'npm run dev'

--- ebay/app/Http/Controllers/ItemController.php
+++ ebay/app/Http/Controllers/ItemController.php
@@ -9,24 +9,17 @@ class ItemController extends Controller
 {
     public function get_all_images()
     {
-	$items = Item::all();
-    $title = "";
-    $txt2 = "<br>";
-	foreach ($items as $item) {
-    	$title = $title.$item->Title.$txt2;
-	};
- 	
- 	return $title.$txt2 ;
+       
     }
    
     public function index(){
  
         $items = Item::all();
- 
         return view('item.item',compact('items'));
     }
  
     public function create(){
+
         return view('item.createItem');
     }
  
