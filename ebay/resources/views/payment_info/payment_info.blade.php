63c64c07300f3f8f315fb2f7fbca82f69a77eeac
sosthene <sosthenedelamotte@gmail.com>
2020-04-17 00:59:44 +0200

    bootstrap on payment

--- ebay/resources/views/payment_info/payment_info.blade.php
+++ ebay/resources/views/payment_info/payment_info.blade.php
@@ -5,36 +5,44 @@
 <h1>page payment test </h1>
 
 @foreach ($payement_infos as $payment_info)
-<li> {{ $payment_info->cardType}} </li>
-<li> {{ $payment_info->cardNumber}} </li>
-<li> {{ $payment_info->cardName}} </li>
-<li> {{ $payment_info->expirationDate}} </li>
-<li> {{ $payment_info->securityCode}} </li>
-
+<ul class="list-group list-group-flush">
+<li class="list-group-item"> {{ $payment_info->cardType}} </li>
+<li class="list-group-item"> {{ $payment_info->cardNumber}} </li>
+<li class="list-group-item"> {{ $payment_info->cardName}} </li>
+<li class="list-group-item"> {{ $payment_info->expirationDate}} </li>
+<li class="list-group-item"> {{ $payment_info->securityCode}} </li>
+</ul>
 <form method="POST" action="/payment/{{$payment_info->id}}">
     {{ csrf_field() }}
     {{ method_field("DELETE") }}
-
-    <button style="danger" type="submit" >delete</button>
+    <br>
+    <button class="btn btn-outline-danger" type="submit" >delete</button>
+    <a href="{{ url('/payment/update_payment/' . $payment_info->id) }}" style="float: right;" class="btn btn-xs btn-info pull-right">Edit</a>
+    <br>
 </form>
+<br> <br>
 
-
-<a href="{{ url('/payment/update_payment/' . $payment_info->id) }}" class="btn btn-xs btn-info pull-right">Edit</a>
 
 
 @endforeach
-
-Add a new one
+<br> <br> <br>
+<h2>Add a new one</h2>
 <form method="POST" action="/payment/create">
     {{ csrf_field() }}
-    
-    <input type="text" placeholder=" cardType" name="cardType" required>
-    <input type="text" placeholder=" cardNumber" name="cardNumber" required>
-    <input type="text" placeholder=" cardName"  name="cardName" required>
-    <input type="date" id="start_date_payment" placeholder=" expirationDate" name="expirationDate" required>
-    <input type="number" placeholder=" securityCode" name="securityCode" required>
 
-    <button style="primary" type="submit" >Create</button>
+
+
+    <input class="form-control" type="text" placeholder=" cardType" name="cardType" required><br>
+    
+    <input class="form-control" type="text" placeholder=" cardName"  name="cardName" required><br>
+    <input class="form-control" type="number" placeholder=" cardNumber" name="cardNumber" required><br>
+    <input class="form-control"type="date" id="start_date_payment" placeholder=" expirationDate" name="expirationDate" required><br>
+    <input class="form-control" type="number" placeholder=" securityCode" name="securityCode"  aria-describedby="cardhelp" required><br>
+    <small id="cardhelp" class="form-text text-muted">We'll never share your private informations with anyone else.</small><br>
+
+    <button class="btn btn-outline-success" type="submit" >Create</button>
+
+
 </form>
 
 @endsection
