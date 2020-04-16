ddaf58e64c2973595834939cff21ca0f76e78b9b
Willy <6wmartin@gmail.com>
2020-04-16 10:58:09 +0200

    Update display offer (item to buy)

--- ebay/resources/views/item/item.blade.php
+++ ebay/resources/views/item/item.blade.php
@@ -7,7 +7,7 @@
         @php
             $item=$items[0];
         @endphp
-        <div class="well" style="display: flex; border: 1px grey solid; padding: 10px; margin: 50px;">
+        <div class="well" style="display: flex; border: 1px grey solid;padding: 10px; margin: 50px; ">
             <div style="margin: 20px; width: 100%;">
                 <h3>{{$item->Title}}</h3>
                 
@@ -18,49 +18,58 @@
 
             
 
-            <div class="right" style=" margin: 20px; border: 1px grey solid; width: 60vh; height: 25vw;">
+            <div class="right" style=" margin: 20px;  width: 30vw; height: 18vw;">
                 <div class="carrousel" >
                     <!--width="500" height="300"-->
                     @foreach($items as $image)
-                        <img  style="width: 40vw; height: 25vw; display: none;" src="/storage/{{$image->reference}}" alt="{{$image->Title}}" > 
+                        <img  style="width: 30vw; height: 18vw; border: 1px grey solid; display: none;" src="/storage/{{$image->reference}}" alt="{{$image->Title}}" > 
                     @endforeach
                 </div>
                 
             </div>
         </div>
 
-        <div style=" background: lightgrey;  margin: 20px;">
-            <p>Disponible : </p>
+        <div style=" background: lightgrey;  margin: 20px 50px; padding: 20px;">
+           
             @if(strpos($item->sell_type, "enchere")!== false)
-                <h4>Vente aux enchères</h4>
-                <p>Prix actuel : <strong>{{$item->Initial_Price}}€</strong>
-                <br> Date Début : {{$item->start_date}}
-                <br> Fin Début : {{$item->end_date}}
-                </p>
-                <p>Enchère automatique :</p>
-                Saissisez le montant maximum que vous etes pret à dépenser: <input type="number" min="{{$item->Initial_Price}}" name="price" id="" placeholder="00€00">
-                <button class="btn btn-primary">Encherire</button>
+                <form action="" method="post">
+                    <h4>Vente aux enchères</h4>
+                    <p>Prix actuel : <strong>{{$item->Initial_Price}}€</strong>
+                    <br>Date Début : {{$item->start_date}}
+                    <br>Fin Début : {{$item->end_date}}
+                    </p>
+                    <h5>Enchère automatique :</h5>
+                    Saissisez le montant maximum que vous etes prêt à dépenser: <input type="number" min="{{$item->Initial_Price}}" name="price" id="" placeholder="00€00">
+                    <input type="submit" class="btn btn-primary" value="Encherire"></input>
+                </form>
             @endif
+
             @if(strpos($item->sell_type, "bestoffer")!== false)
-                <h4>Vente au meilleur prix</h4>
-                <p> Proposez le prix que vous souhaitez au vendeur !</p>
-                <input type="number" name="price" id="" placeholder="00€00">
-                <button class="btn btn-primary">Faire une proposition</button>
+            
+                <form action="" method="post">
+                    <h4>Vente au meilleur prix</h4>
+                    <span> Proposez le prix que vous souhaitez au vendeur : </span>
+                    <input type="number" name="price" id="" placeholder="00€00">
+                    <input type="submit" class="btn btn-primary" value="Faire une proposition">
+                </form>
+                
             @endif
+
             @if(strpos($item->sell_type, "immediat")!== false)
-                <h4>Vente immediate</h4>
-                <p> Prix : {{$item->Initial_Price}}€ </p>
-                <button class="btn btn-primary">Achat immediat</button>
+            
+                <form action="" method="post">
+                    <h4>Vente immediate</h4>
+                    <span> Prix : {{$item->Initial_Price}}€ </span>
+                    <input type="submit" class="btn btn-primary" value="Ajouter à mon panier">
+                </form>
             @endif
         </div>
-               
-
-            
-        
+ 
     @else
         <p> Nous n'avons trouvé aucun article correspondant.
             <br> Nous vous invitons à revenir plus tard. 
         </p>
     @endif
+
 @endsection
 
